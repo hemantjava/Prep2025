4f419da03ed31b8ddfea18424842abdb3f7c0db6
Hemant <hemantjava90@gmail.com>
2025-08-21 23:59:54 +0530

    SOLID principles

---
+++ src/main/java/com/prep/interview/recurson/SumOfNumber.java
@@ -0,0 +1,29 @@
+package com.prep.interview.recurson;
+
+public class SumOfNumber {
+
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(recursiveSum(num));
+    }
+
+    private static int recursiveSum(int num) {
+        if (num<= 0)
+            return 0;
+        return num + recursiveSum(num -1);
+    }
+
+    public static void printDecreasingOrder(int num) {
+        if (num <= 0)
+            return;
+        System.out.print(num+" ");
+        printDecreasingOrder(num - 1);
+    }
+    public static void printIncreasingOrder(int num) {
+        if (num <= 0)
+            return;
+        printIncreasingOrder(num - 1);
+        System.out.print(num+" ");
+    }
+}
